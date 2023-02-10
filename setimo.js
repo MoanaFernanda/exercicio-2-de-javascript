95f346b5bf74fa66a1f3d2f98b0cac4b9cf97cd9
Moana Fernanda <101598428+MoanaFernanda@users.noreply.github.com>
2023-02-09 21:04:06 -0300

    Add files via upload

---
+++ setimo.js
@@ -0,0 +1,16 @@
+function imc(a, b){
+    let x;
+    x = b/(a*a);
+    console.log(x);
+    if(x<18.5){
+        return "Baixo peso";
+    }else if(x>=18.5 && x<25){
+        return "Peso normal";
+    }else if(x>=25 && x<30){
+        return "Excesso de peso";
+    }else if(x>=30 && x<35){
+        return "Obesidade";
+    }else if(x>=35){
+        return "Obesidade extrema";
+    }
+}
